9eaca07def4d365b4d42a47456099dd40c2fdbc8
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-07-04 21:13:18 -0300

    ajustes

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,20 +2,22 @@ import styles from './Prato.module.scss';
 import { useParams, useNavigate } from 'react-router-dom';
 import cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = cardapio.find(item => item.id === Number(id));
-  if(!prato) {
-    return '';
+  if (!prato) {
+    return <NotFound />;
   }
   return (
-    <>
+    <PaginaPadrao>
       <button className={styles.voltar} onClick={() => navigate(-1)}>
         {'< Voltar'}
       </button>
-      <div className={styles.container}>
+      <section className={styles.container}>
         <h1 className={styles.titulo}>
           {prato.title}
         </h1>
@@ -26,9 +28,9 @@ export default function Prato() {
           <p className={styles.conteudo__descricao}>
             {prato.description}
           </p>
+          <TagsPrato {...prato} />
         </div>
-        <TagsPrato {...prato} />
-      </div>
-    </>
+      </section>
+    </PaginaPadrao>
   );
 }
